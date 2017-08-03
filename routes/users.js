0ac80529a7893800f0dd85063652b3624b7c32e7
覃超成 <meimaonong@163.com>
2017-08-03 22:14:53 +0800

    添加

--- routes/users.js
+++ routes/users.js
@@ -19,6 +19,29 @@ router.get('/success', function(req, res, next) {
   res.render('users/success');
 });
 
+router.get('/edit', function(req, res, next) {
+  // res.console.log("Hello from your server :)");
+  // res.render('index', { title: 'Express' });
+  var id = req.query.id;
+  userModel.findOne({_id: id}, function (err, data) {
+    res.console.log(data);
+    res.render('users/edit', {
+      user: data
+    })
+  })
+
+  //res.render('users/edit');
+});
+
+router.post('/delete', function (req, res) {
+  var id = req.body.id;
+  //res.console.log(req);
+  userModel.remove({_id: id}, function (err, data) {
+    if(err){ return console.log(err); }
+    res.json({code: 200, msg: '删除成功'});
+  })
+})
+
 router.post('/add', function(req, res, next) {
   var newUser = new userModel({
     email: req.body.email,
@@ -27,7 +50,30 @@ router.post('/add', function(req, res, next) {
   })
   newUser.save(function(err, data){
     if(err){ return console.log(err) }
-    res.redirect('/users/success');
+    //res.redirect('/users/success');
+    res.send({code: 0, data: null, msg: '添加成功'})
+  })
+});
+
+router.post('/update', function (req, res, next) {
+  var id = req.body.id;
+  userModel.findById(id, function (err, data) {
+    if(err){ return console.log(err); }
+    data.password = req.body.password;
+    data.email = req.body.email;
+    data.save(function(err){
+      res.json({code: 200, msg: '更新成功'});
+    })
+  })
+});
+
+router.get('/list', function(req, res, next) {
+  userModel.find(function(err, data){
+    if(err){ return console.log(err) }
+    res.console.log(data)
+    res.render('users/index',{
+      user: data
+    })
   })
 });
 
